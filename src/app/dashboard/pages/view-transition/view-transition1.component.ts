b9ea9578b55117608386dd3034653a65f043e0de
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-06-17 15:58:15 +0200

    "408"

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -13,9 +13,13 @@ import { TitleComponent } from '@shared/title/title.component';
     alt="Picsum"
     width="200"
     height="300"
+    style="view-transition-name: hero1"
     />
 
-    <div class="bg-blue-500 w-56 h-56"></div>
+    <div
+    style="view-transition-name: hero2"
+     class="bg-blue-500 w-56 h-56"
+     ></div>
 
     </section>
       `
